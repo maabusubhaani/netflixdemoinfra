4cb005dab07152fc3fbe80d38a0e44a27b9438e2
maabusubhaani <subhaanis61@gmail.com>
2025-03-26 14:22:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ region = "us-east-1"
 }
 
 resource "aws_instance" "one" {
-count = 5
-ami = "ami-0ddc798b3f1a5117e"
+count = 4
+ami = "ami-04aa00acb1165b32a"
 instance_type = "t2.medium"
-key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "subhaanikeypair"
+vpc_security_group_ids = ["sg-07c30bf900a4cab91"]
 tags = {
 Name = var.instance_names[count.index]
 }
